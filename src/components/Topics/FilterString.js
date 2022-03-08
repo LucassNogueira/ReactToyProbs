cedfa5a6f49020b677de2ae9c7b8b66c81e132df
LucassNogueira <simplylucasjason@gmail.com>
2022-03-08 10:34:38 -0600

    refactor filterstring

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -16,30 +16,30 @@ export default class FilterString extends Component {
       filteredArray: [],
     };
   }
+  handleChange = (e) => {
+    this.setState({ userInput: e.target.value });
+  };
+  evaluate = () => {
+    let word = this.state.userInput;
+    let names = this.state.unFilteredArray;
+    let filtered = [];
+    // if (names.includes(word)) {
+    //   filtered.push(word);
+    // }
+    names.forEach((el) =>
+      el.includes(word) ? filtered.push(el) : console.log("not included")
+    );
+    return this.setState({ filteredArray: filtered });
+  };
   render() {
-    const handleChange = (e) => {
-      this.setState({ userInput: e.target.value });
-    };
-    const evaluate = () => {
-      let word = this.state.userInput;
-      let names = this.state.unFilteredArray;
-      let filtered = this.state.filteredArray;
-      // if (names.includes(word)) {
-      //   filtered.push(word);
-      // }
-      names.forEach((el) =>
-        el.includes(word) ? filtered.push(el) : console.log("not included")
-      );
-      this.setState(this.state);
-    };
     return (
       <div className="puzzleBox filterStringPB">
         <h4>Filter String</h4>
         <span className="puzzleText">
           Stuff :{JSON.stringify(this.state.unFilteredArray)}
         </span>
-        <input className="inputLine" type="text" onChange={handleChange} />
-        <button onClick={evaluate} className="confirmationButton">
+        <input className="inputLine" type="text" onChange={this.handleChange} />
+        <button onClick={this.evaluate} className="confirmationButton">
           Is this name included?
         </button>
         <span className="resultsBox filterStringRB">
